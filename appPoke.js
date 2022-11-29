76c7082f2dbc3fcdea0c1b9dcecbcce80b737acb
Osefx <cinci57450@gmail.com>
2022-11-29 21:45:51 +0100

    get all pokemons

--- appPoke.js
+++ appPoke.js
@@ -5,6 +5,7 @@ const appPoke = express()
 const port = 4200
 
 appPoke.get('/', (req, res) => res.send('hello express 2 !'))
+
 appPoke.get('/pokemons/:id/', (req, res) => {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
@@ -12,8 +13,19 @@ appPoke.get('/pokemons/:id/', (req, res) => {
     res.json(success(message, pokemon))
     // res.send(`pokemon numero ${id}, nom : ${pokemon.name}, image : ${pokemon.picture}`)
 })
+
+appPoke.get('/pokemons', (req, res) => {
+    const pokemon = pokemons.length
+    const message = 'Get ALL POKEMONS in the DB'
+    res.json(success(message,pokemons))
+})
+
 appPoke.get('/pokemonscount', (req, res) => {
     const pokemon = pokemons.length
     res.send(`total de pokemon ${pokemon}`)
 })
+
+
+
+
 appPoke.listen(port, () => console.log(`listening on port : ${port}`))
