8dac44395a32233b971602cf25e0c50dcc3a27a2
Osefx <cinci57450@gmail.com>
2022-11-29 21:26:40 +0100

    Fist commit, node tutorial, api rest, pokemon

---
+++ appPoke.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const {success} = require('./helper.js')
+let pokemons = require('./mock-pokemon')
+const appPoke = express()
+const port = 4200
+
+appPoke.get('/', (req, res) => res.send('hello express 2 !'))
+appPoke.get('/pokemons/:id/', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    const message = 'Pokemon found'
+    res.json(success(message, pokemon))
+    // res.send(`pokemon numero ${id}, nom : ${pokemon.name}, image : ${pokemon.picture}`)
+})
+appPoke.get('/pokemonscount', (req, res) => {
+    const pokemon = pokemons.length
+    res.send(`total de pokemon ${pokemon}`)
+})
+appPoke.listen(port, () => console.log(`listening on port : ${port}`))
---
+++ helper.js
@@ -0,0 +1,6 @@
+exports.success = (message, data) => {
+    return {
+        message,
+        data
+    }
+}
