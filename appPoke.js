840cca9b2965a7625a453a34865c6e8d7b2bc8be
Osefx <cinci57450@gmail.com>
2022-11-29 22:31:26 +0100

    add of morgan and favicon

--- appPoke.js
+++ appPoke.js
@@ -1,13 +1,20 @@
 const express = require('express')
-const {success} = require('./helper.js')
-const {logger, loggerURL} = require('./logger.js')
+const morgan = require('morgan')
+const favicon = require('serve-favicon')
+const { success } = require('./helper.js')
+const { loggerURL } = require('./logger.js')
 let pokemons = require('./mock-pokemon')
 const appPoke = express()
 const port = 4200
 
-appPoke.use(loggerURL)
+appPoke
+    .use(loggerURL)
+    .use(favicon(__dirname + '/icone.ico'))
+appPoke.use(morgan('dev'));
 
-appPoke.get('/', (req, res) => res.send('hello express 2 !'))
+appPoke.get('/', (req, res) => 
+    res.send('hello express 2 !')
+);
 
 appPoke.get('/pokemons/:id/', (req, res) => {
     const id = parseInt(req.params.id)
@@ -15,20 +22,20 @@ appPoke.get('/pokemons/:id/', (req, res) => {
     const message = 'Pokemon found'
     res.json(success(message, pokemon))
     // res.send(`pokemon numero ${id}, nom : ${pokemon.name}, image : ${pokemon.picture}`)
-})
+});
 
 appPoke.get('/pokemons', (req, res) => {
     const pokemon = pokemons.length
     const message = 'Get ALL POKEMONS in the DB'
     res.json(success(message,pokemons))
-})
+});
 
 appPoke.get('/pokemonscount', (req, res) => {
     const pokemon = pokemons.length
     res.send(`total de pokemon ${pokemon}`)
-})
+});
 
 
-
-
-appPoke.listen(port, () => console.log(`listening on port : ${port}`))
+appPoke.listen(port, () => 
+    console.log(`listening on port : ${port}`)
+    );
