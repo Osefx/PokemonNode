5e0f5d23c752231dc74dd980d18c18f7442d29fe
Osefx <cinci57450@gmail.com>
2022-11-30 22:32:35 +0100

    crud base (post, put, delete)

--- appPoke.js
+++ appPoke.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const favicon = require('serve-favicon')
+const bodyParser = require('body-parser')
 const { success } = require('./helper.js')
 const { loggerURL } = require('./logger.js')
 let pokemons = require('./mock-pokemon')
@@ -8,15 +9,16 @@ const appPoke = express()
 const port = 4200
 
 appPoke
-    .use(loggerURL)
+    //.use(loggerURL)
     .use(favicon(__dirname + '/icone.ico'))
-appPoke.use(morgan('dev'));
+    .use(morgan('dev'))
+    .use(bodyParser.json());
 
 appPoke.get('/', (req, res) => 
     res.send('hello express 2 !')
 );
 
-appPoke.get('/pokemons/:id/', (req, res) => {
+appPoke.get('/app/pokemons/:id/', (req, res) => {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
     const message = 'Pokemon found'
@@ -24,7 +26,7 @@ appPoke.get('/pokemons/:id/', (req, res) => {
     // res.send(`pokemon numero ${id}, nom : ${pokemon.name}, image : ${pokemon.picture}`)
 });
 
-appPoke.get('/pokemons', (req, res) => {
+appPoke.get('/app/pokemons', (req, res) => {
     const pokemon = pokemons.length
     const message = 'Get ALL POKEMONS in the DB'
     res.json(success(message,pokemons))
@@ -35,6 +37,36 @@ appPoke.get('/pokemonscount', (req, res) => {
     res.send(`total de pokemon ${pokemon}`)
 });
 
+// ajout d un pokemon
+appPoke.post('/app/pokemons', (req, res) => {
+    // const jsonParser = bodyParser.json()
+    const id = pokemons.length + 1
+    const pokemonCreate = { ...req.body, ...{id: id, created: new Date()}} 
+    //const jsonPokemonCreate = jsonParser.pokemonCreate
+    pokemons.push(pokemonCreate)
+    const message = `Le pokemon ${pokemonCreate.name} a bien été créé`
+    res.json(success(message, pokemonCreate))
+});
+// update pokemon
+appPoke.put('/app/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const pokemonUpdate = { ...req.body, id: id }
+    pokemons = pokemons.map(pokemon => {
+        return pokemon.id === id ? pokemonUpdate : pokemon
+    })
+    const message = `Le pokémon ${pokemonUpdate.name} a bien été modifié.`
+    res.json(success(message, pokemonUpdate))
+});
+// delete pokemon
+appPoke.delete('/app/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemonDelete = pokemons.find(pokemon => 
+        pokemon.id === id)
+    pokemons = pokemons.filter(pokemon => 
+        pokemon.id !== id)
+    const message = `Le pokémon ${pokemonDelete.name} a bien été supprimé.`
+    res.json(success(message, pokemonDelete))
+});
 
 appPoke.listen(port, () => 
     console.log(`listening on port : ${port}`)
